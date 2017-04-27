45d2c3f6bb6da6029d622286269b793c10587838
Luqman Malik <luqmanmalik@hilixr.com>
2017-04-27 11:11:38 -0700

    Hello, Haskell!
    ...

---
+++ haskell-tut.hs
@@ -0,0 +1,26 @@
+import Data.List
+import System.IO
+
+
+-- Data types
+
+-- Int [-2^63, 2^63]
+maxInt = maxBound :: Int
+minInt = minBound :: Int
+
+-- Integer : unbounded whole number == size of computer memory == 1234567891011..
+
+-- Float : single precision floating point numbers : a.b
+
+-- Double : precision \= 11 decimal points
+bigFloat = 3.99999999999 + 0.00000000005
+
+-- Declaare permanent value of a variable
+always5 :: Int
+always5 = 5
+
+-- Car 'x'
+
+-- Lists [a,b,c,...,n], Note: a string is just a list of characters
+
+
